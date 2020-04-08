cdc7805e25aefcce0ce44723c1675b44639b0b92
Timothy Itodo <itodotimothy6@gmail.com>
2020-04-08 14:13:51 -0500

    init commit

---
+++ github.js
@@ -0,0 +1,22 @@
+class Github {
+  constructor() {
+    this.client_id = 'ef0d523cecbaa3b095d4';
+    this.client_secret = '3de595c6190582ff889c1cea1d4017bde364bc30';
+    this.repos_count = 5;
+    this.repos_sort = 'created: asc';
+  }
+
+  async getUser(user) {
+    const profileResponse = await fetch(`https://api.github.com/users/${user}?client_id=${this.client_id}&client_secret=${this.client_secret}`);
+
+    const repoResponse = await fetch(`https://api.github.com/users/${user}/repos?per_page=${this.repos_count}&sort=${this.repos_sort}&client_id=${this.client_id}&client_secret=${this.client_secret}`);
+
+    const profile = await profileResponse.json();
+    const repos = await repoResponse.json();
+
+    return {
+      profile,
+      repos
+    }
+  }
+}
